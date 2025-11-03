e65dd03bc8ceb7aeafe9cf61dc45ee71caa22c20
vanhungn <vanhungnvh1712004@gmail.com>
2025-11-03 09:49:50 +0700

    ok

--- config/db.js
+++ config/db.js
@@ -4,9 +4,9 @@ const ConnectData = async () => {
         await mongoose.connect("mongodb+srv://hungnguyenninhbinh2004_db_user:wgi1ikeOGrzmCDPj@thuctap.gtxo4lu.mongodb.net/thucTap?appName=ThucTap")
 
         console.log("Connect success")
-        
+
     } catch (error) {
         console.log("can not connect")
     }
 }
-module.exports =ConnectData
+module.exports = ConnectData
